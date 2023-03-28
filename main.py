f317a03dc120099b2bb46844ab37425cadcf90d0
seokb <greensode@gmail.com>
2023-03-28 17:42:08 +0900

    커밋 2 그레이스케일 추가

--- main.py
+++ main.py
@@ -3,10 +3,16 @@ import cv2
 # 영상 소스 열기
 src = cv2.imread('images/picture01.jpg')
 
+## 영상 처리 알고리즘 구현 ##
+dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
+
+
+
+
 
 #영상 디스플레이
 cv2.imshow('src',src)
-
+cv2.imshow('dst1',dst1)
 
 #마무리
 cv2.waitKey(0)
