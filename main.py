d888cbd5c876bc5cf8e85ec684cc9d6133364d22
seokb <greensode@gmail.com>
2023-03-28 17:44:32 +0900

    커밋 3 새로운 영상 처리 추가

--- main.py
+++ main.py
@@ -5,14 +5,15 @@ src = cv2.imread('images/picture01.jpg')
 
 ## 영상 처리 알고리즘 구현 ##
 dst1 = cv2.cvtColor(src, cv2.COLOR_RGB2GRAY)
-
-
+dst2 = src.copy()
+_,dst2 = cv2.threshold(dst2, 127,255,cv2.THRESH_BINARY)
 
 
 
 #영상 디스플레이
 cv2.imshow('src',src)
 cv2.imshow('dst1',dst1)
+cv2.imshow('dst2',dst2)
 
 #마무리
 cv2.waitKey(0)
